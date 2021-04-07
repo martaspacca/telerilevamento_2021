07259ed5a585e454e3fcbf20524a84c3b55529f2
martaspacca <80707907+martaspacca@users.noreply.github.com>
2021-04-07 23:33:57 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -6,3 +6,26 @@
 library(raster)
 
 setwd("C:/lab/greenland")
+
+#importiamo il primo dataset
+lst_2000 <- raster("lst_2000.tif")
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+#lo plottiamo
+plot(lst_2005) 
+
+#par(mfrow=c(2,2))
+par(mfrow=c(2,2))
+lst_2000 <- raster("lst_2000.tif")
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+#list.files
+
+rlist <- list.files(pattern="lst")
+rlist
+
+#lapply R package
