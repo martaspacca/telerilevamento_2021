755859be2b227c6385cb39e4e9876ea452687dcd
martaspacca <80707907+martaspacca@users.noreply.github.com>
2021-04-08 22:59:02 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -13,19 +13,31 @@ lst_2005 <- raster("lst_2005.tif")
 lst_2010 <- raster("lst_2010.tif")
 lst_2015 <- raster("lst_2015.tif")
 
-#lo plottiamo
-plot(lst_2005) 
+#plot dataset
+plot(lst_2000) 
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
 
 #par(mfrow=c(2,2))
 par(mfrow=c(2,2))
-lst_2000 <- raster("lst_2000.tif")
-lst_2005 <- raster("lst_2005.tif")
-lst_2010 <- raster("lst_2010.tif")
-lst_2015 <- raster("lst_2015.tif")
+plot(lst_2000) 
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
 
-#list.files
-
+#list.files: creaiamo la lista di files a cui apllicare Lapply
+#pattern: i files devono avere un nome in comune, an optional regular expression
 rlist <- list.files(pattern="lst")
 rlist
 
-#lapply R package
+#lapply: apply a Function over a List or Vector
+lapply(rlist,raster)
+import<-lapply(rlist,raster)
+
+#stack: impacchettiamo i nostri files raster
+TGr<-stack(import)
+
+plot(Tgr)
+plotRGB(TGr, 1, 2, 3, stretch="Lin")
+
